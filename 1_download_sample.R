aa45eec269a69a30dfbde9124f0b3ee67fea80be
Martin Holst <medi@Martins-MacBook-Pro.local>
2021-12-02 21:02:32 +0100

    further work on processing

--- 1_download_sample.R
+++ 1_download_sample.R
@@ -1,9 +1,10 @@
 library(tidyverse)
+library(cthist)
 
 set.seed(946)
 
 ## download the complete IntoValue 1 and 2 datasets from GitHub
-## downloaded on 10-11-2021
+## last downloaded on 02-12-2021
 data <- read_csv('https://raw.githubusercontent.com/maia-sh/intovalue-data/main/data/processed/trials.csv')
 
 ## apply our predefined inclusion criteria and count the rows
@@ -19,19 +20,16 @@ sample <- included_data %>%
   sample_n(25) # pilot sample
 
 ## save the entries from ClinicalTrials.gov and DRKS separately
-sample %>%
-  rename(nctid = 'id') %>%
-  filter(registry == "ClinicalTrials.gov") %>%
-  write_csv('nctids.csv')
-sample %>%
-  rename(drksid = 'id') %>%
-  filter(registry == "DRKS")  %>%
-  write_csv('drksids.csv')
+sample_ct <- sample %>%
+  filter(str_detect(id, 'NCT')) %>%
+  unlist() %>%
+  as.vector()
+sample_DRKS <- sample %>%
+  filter(str_detect(id, 'DRKS')) %>%
+  unlist() %>%
+  as.vector()
 
-## The full historical versions of trial registry entries in this
-## sample must be downloaded using the clinicaltrials.gov.R and the
-## DRKS.de.R scripts found here:
-## https://codeberg.org/bgcarlisle/ClinicalTrialsHistoryScraper
-## When finished, save the resulting CSV files to this directory as:
-## historical_versions_ct.csv
-## historical_versions_DRKS.csv
+## download the historical versions from ClinicalTrials.gov and DRKS separately
+clinicaltrials_gov_download(sample_ct, 'historical_versions_ct.csv')
+drks_de_download(sample_DRKS, 'historical_versions_DRKS.csv')
+
--- 2_process_data.R
+++ 2_process_data.R
@@ -1,17 +1,20 @@
 library(tidyverse)
+library(jsonlite)
 
 ## Read in the files that contain all historical versions of trials
 ## in our sample
-## downloaded on 10-11-2021 (piloting)
+## downloaded on 02-12-2021 (piloting)
 data_ct <- read_csv("historical_versions_ct.csv")
 data_drks <- read_csv("historical_versions_DRKS.csv")
 
 ## separate for each dataset (to ease merging them), drop all variables
 ## that we do not need and mutate others
 data_ctxx <- data_ct %>%
-  select(!c(enrolment, enrolment_type, min_age, max_age, sex, gender_based, accepts_healthy_volunteers, contacts)) %>%
-  mutate(str_split_fixed(outcome_measures, 'Secondary Outcome Measures', n = 2)) ## does not work
-## not yet sure how to do this
+  select(!c(enrolment, enrolment_type, min_age, max_age, sex, gender_based, accepts_healthy_volunteers, contacts, sponsor_collaborators)) %>%
+  separate(col = outcome_measures, into = c(primary_outcomes, secondary_outcomes), sep = ',{"section":"Secondary Outcome Measures:"', extra = 'merge', ) # works?
+## some other code chunks
+## mutate(primary_outcomes = str_split_fixed(outcome_measures, 'Secondary Outcome Measures', n = 2))
+## mutate(str_split_fixed(outcome_measures, 'Secondary Outcome Measures', n = 2)) ## does not work
 data_drks <- data_drks %>%
   select(!c(enrolment, enrolment_type, min_age, max_age, gender, contacts)) %>%
   mutate(criteria = paste('INCLUSION CRITERIA:', inclusion_criteria, 'EXCLUSION_CRITERIA:', exclusion_criteria)) %>%
@@ -20,4 +23,11 @@ data_drks <- data_drks %>%
 
 ## just to check
 data_ctxx %>% write_csv('testcwt.csv')
-data_drks %>% write_csv('testdrks.csv')
+data_ctxxxxx <- prettify(data_ct)
+
+
+## TO DO:
+## rename overall_status (ct) and recruitment_status (DRKS) to status
+## (DRKS) start_date to study_start_date
+## primary_completion_date (ct) and closing_date (DRKS) to completion_date
+## primary_completion_date_type - there need to be NAs in DRKS, as it does not have that variable
