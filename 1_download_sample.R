284124b92c236c27b8a3f438dacc0a227db225c9
Martin Holst <medi@Martins-MacBook-Pro.local>
2022-01-17 21:58:42 +0100

    set up automated analyses for all trials

--- 1_download_sample.R
+++ 1_download_sample.R
@@ -14,6 +14,10 @@ dat_IV_included <- dat_IV %>%
 nrow(dat_IV_included)
 # 1800
 
+
+
+## ---- OLD STRATEGY: draw a sample of 25 ----
+
 ## draw a sample, at first for piloting
 sample_nctids <- dat_IV_included %>%
   select(id) %>%
@@ -40,6 +44,35 @@ dat_IV_sample <- dat_IV_included %>%
   filter(!(is_dupe == TRUE & iv_version == 1)) # remove duplicates that are present in both IntoValue versions
 dat_IV_sample %>%
   write_csv('data/sample_IntoValue.csv')
+
+
+
+## ---- NEW STRATEGY: run history scraper on all included trials ----
+
+## select IDs
+ids <- dat_IV_included %>%
+  select(id)
+
+## split the entries from ClinicalTrials.gov and DRKS
+ids_ct <- ids %>%
+  filter(str_detect(id, 'NCT')) %>%
+  unlist() %>%
+  as.vector()
+ids_DRKS <- ids %>%
+  filter(str_detect(id, 'DRKS')) %>%
+  unlist() %>%
+  as.vector()
+
+## download the historical versions from ClinicalTrials.gov and DRKS separately
+## (insert last download date here, maybe)
+clinicaltrials_gov_download(ids_ct, 'data/historical_versions_ct_all.csv')
+drks_de_download(ids_DRKS, 'data/historical_versions_DRKS_all.csv')
+
+## save the IntoValue sample, too
+dat_IV_no_dupes <- dat_IV_included %>%
+  filter(!(is_dupe == TRUE & iv_version == 1)) # remove duplicates that are present in both IntoValue versions
+dat_IV_no_dupes %>%
+  write_csv('data/data_IntoValue_no_dupes.csv')
 
 ## save the sample nctids for possible later use
 ## to extract PMIDs (deprecated)
